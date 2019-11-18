8b9eb558a997c2b8b3a12c6e5cba4d6589bd4d9c
José Costa <jose.fernando.costa.1998@gmail.com>
2019-11-18 11:56:39 +0000

    Added a NodeJs pagination tutorial

---
+++ examples/NodeJS/pagination_tutorial/users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
